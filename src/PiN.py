4951f422804cc11e30aa89a23aec5248a27cf77a
MartinMikkelsen <MartinMikkelsen@post.au.dk>
2022-04-03 21:06:38 +0200

    shooting

--- src/PiN.py
+++ src/PiN.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from scipy.integrate import trapz
 from scipy.optimize import root
 from scipy.special import spherical_jn
+from scipy.integrate import solve_bvp
 import seaborn as sns
 sns.set_style("dark")
 sns.set(font_scale=1)
@@ -15,38 +16,32 @@ S = 10    #MeV
 m = 139.570   #MeV
 mn = 939.5  #MeV
 mu = m*mn/(mn+m) #Reduced mass
-#hbar = 6.582*10**(-22) #MeV s
 g = (2*mu)
 
 def f(r): #form factor
     return S*np.exp(-r**2/b**2)
 
-def diff(phi,r,E):
-    return (phi[1],-(E-m)*phi[0]-2/r*phi[1]+f(r))
+def sys(r,u,E):
+    y,v,I = u
+    dy = v
+    dv = g*(-E+m)*y-2/r*v+g*f(r)
+    dI = f(r)*r**4*y
+    return dy,dv,dI
 
-phi0 = [b/m,b/m] #Initial
+def bc(ua, ub,E):
+    ya,va,Ia = ua
+    yb,vb,Ib = ub
+    return va, vb+(g*(m+abs(E)))**0.5*yb, Ia, Ib-E
 
-def phi_fun(E):
-    rs = np.linspace(1e-5,50,10000)
-    ys = odeint(lambda phi,r: diff(phi,r,E), phi0, rs)
-    integral = 12*np.pi*trapz(ys[:,0]*f(rs)*rs**4,rs)
-    return integral - E
+r = np.logspace(-5,0,20)*5
+u = [0*r,0*r,E*r/r[-1]]
+E = -2
+res = solve_bvp(sys,bc,r,u,p=[E],tol=1e-5)
+print(res.message,", E: ",res.p[0])
 
-E_true = root(phi_fun, 20).x
-
-rs = np.linspace(1e-5,50,10000)
-ys = odeint(lambda phi,r: diff(phi,r,E_true), phi0, rs)
-
-phi_true = ys[:,0]
-
-plt.plot(rs, phi_true,linewidth=2,label=r'$\phi(x)$')
-print("Minimum found at E =",E_true)
-
-plt.title("Numerical solution",fontsize=14)
-plt.xlabel("r [fm]",fontsize=14)
-#plt.xticks([0,2,4,6,8,10],fontsize=14)
-#plt.yticks([-0.2,0,0.2,0.4,0.6,0.8,1],fontsize=14)
-plt.legend(loc=0,fontsize=14)
-plt.figure(figsize=(10,6))
-plt.savefig("phi.png")
+plt.plot(res.x,res.y.T,'--',linewidth=2.5);
+plt.title("Numerical solution")
+plt.grid(); plt.legend(r"$\phi$ $\phi'$ $I$".split());
+plt.xlabel("r [fm]")
+plt.savefig("Integralplot.pdf", format="pdf", bbox_inches="tight")
 plt.show()
